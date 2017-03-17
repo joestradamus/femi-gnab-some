25b7af9b3e7e6891b6c431a45f2c59eb5a4c9321
joemcadams <joseph.mcadams@mu.edu>
2017-03-17 09:59:03 -0600

    Updated logging format in app.js

--- server/app.js
+++ server/app.js
@@ -4,50 +4,24 @@ const nlp = require('compromise')
 const sentiment = require('sentiment')
 const Twit = require('twit')
 const fs = require('fs')
+const jsonFormat = require('json-format')
 
 const credentials = require('../credentials.json')
 const app = express()
 
-const extractDetailsFrom = (tweet) => {
-    const id = tweet.id
-    const date = tweet.created_at
-    const text = tweet.text
-    const textSentiment = sentiment(tweet.text) // get sentiment of text, as well as all positive, negative words
-    const textTopics = nlp(tweet.text).people().data() // get info on sentence's topics (people, places)
-    const user = {
-        id: tweet.user.id,
-        name: tweet.user.name,
-        location: tweet.user.location,
-        followerCount: tweet.user.followers_count,
-        friendsCount: tweet.user.friends_count,
-        favoritesCount: tweet.user.favourites_count,
-        statusesCount: tweet.user.statuses_count,
-        profileImage: tweet.user.profile_image_url
-    }
-    const usersMentioned = tweet.user.user_mentions
-    return {
-        id,
-        date,
-        text,
-        textSentiment,
-        textTopics,
-        user,
-        usersMentioned
-    }
-}
+app.use( (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+})
 
-const logTweet = (tweet) => {
-    const details = extractDetailsFrom(tweet)
-    const logMessage = `${JSON.stringify(details)}`
-    if (details.textSentiment.score !== 0) {
-        fs.writeFile(`server/output/san-francisco/TWEET-${tweet.id}.txt`, logMessage, (err) => {
-            if (err) {
-                return console.log(err)
-            }
-            console.log(`Logged a tweet: [${tweet.id}]`)
-        })
-    }
-}
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'static')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'static', 'public/index.html'))
+})
 
 const logTweetsWithParams = () => {
     const twitter = new Twit(credentials)
@@ -64,20 +38,39 @@ const logTweetsWithParams = () => {
     })
 }
 
-app.use( (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-})
+const logTweet = (tweet) => {
+    const details = extractDetailsFrom(tweet)
+    const logMessage = jsonFormat(details, { type: 'space', size: 2} )
+    if (details.textSentiment.score !== 0) {
+        fs.writeFile(`server/output/san-francisco/TWEET-${tweet.id}.txt`, logMessage, (err) => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log(`Logged a tweet: [${tweet.id}]`)
+        })
+    }
+}
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'static')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'static', 'public/index.html'))
-})
-
+const extractDetailsFrom = (tweet) => {
+    return {
+        id: tweet.id,
+        date: tweet.created_at,
+        text: tweet.text,
+        textSentiment: sentiment(tweet.text), // get sentiment of text, as well as all positive, negative words
+        textTopics: nlp(tweet.text).people().data(), // get info on sentence's topics (people, places)
+        user: {
+            id: tweet.user.id,
+            name: tweet.user.name,
+            location: tweet.user.location,
+            followerCount: tweet.user.followers_count,
+            friendsCount: tweet.user.friends_count,
+            favoritesCount: tweet.user.favourites_count,
+            statusesCount: tweet.user.statuses_count,
+            profileImage: tweet.user.profile_image_url
+        },
+        usersMentioned: tweet.user.user_mentions
+    }
+}
 logTweetsWithParams()
 
 module.exports = app
