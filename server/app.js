770afe9fef89c043f53924953e973d82d409beef
joemcadams <joseph.mcadams@mu.edu>
2017-03-25 20:04:07 -0500

    Build path should be correct for Heroku...

--- server/app.js
+++ server/app.js
@@ -20,13 +20,10 @@ process.env.NODE_ENV === 'development'
 
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, '..', '../build')))
-
-
-app.use(express.static(path.resolve(__dirname, '..', '../build')))
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', '../build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 })
 
 const listenForTweets = () => {
