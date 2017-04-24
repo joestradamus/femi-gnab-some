e77d4801f41c231628b56abd524d8dd6a86e0e92
joemcadams <joseph.mcadams@mu.edu>
2017-04-24 00:20:10 -0500

    Cleaned up notation a bit

--- src/components/stateless/GenderProfile.js
+++ src/components/stateless/GenderProfile.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 import { ProfileCard } from './ProfileCard'
 
-/**
- *  Check for 
- */
 export const GenderProfile = (props) => (
     <div>
         <ProfileCard 
-            text={ `${props.name} users referring to male subject: 
-                ${props.male.sentiment.value/props.male.sentiment.totalCount}`}
+            text={ `${props.name} users referring to male subject
+                Average Sentiment: ${props.male.sentiment.value/props.male.sentiment.totalCount}`}
             allSeries={{
                 positive: props.male.positive,
                 negative: props.male.negative
@@ -17,7 +14,7 @@ export const GenderProfile = (props) => (
         />
         <ProfileCard 
             text={ `${props.name} users referring to female subject
-                ${props.female.sentiment.value/props.female.sentiment.totalCount}`}
+                Average Sentiment: ${props.female.sentiment.value/props.female.sentiment.totalCount}`}
             allSeries={{
                 positive: props.female.positive,
                 negative: props.female.negative
